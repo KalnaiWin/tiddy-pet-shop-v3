4284b1f22ce1dabac3fbefeea34d8b2f30968ac8
Phúc <nhbphuc2006@gmail.com>
2025-10-08 10:44:45 +0700

    Set up for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,22 @@
 import express from "express";
 import "dotenv/config";
+import path from "path";
 
 import authRoute from "./routes/auth.route.js";
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/*splat", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.use("/api/auth", authRoute);
 
