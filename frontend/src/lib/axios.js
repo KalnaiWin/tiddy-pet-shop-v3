37c0629df95573d67e49f2f2b7f22ba8bdc83667
Phúc <nhbphuc2006@gmail.com>
2025-10-18 12:29:14 +0700

    Changed axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
-      : "https://tiddy-pet-shop-v3-backend.vercel.app/api",
+      : "/api",
   withCredentials: true,
 });
